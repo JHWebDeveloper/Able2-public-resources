479bf71ebe8cd2fd823a4846d18eb09af3b2c26a
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2023-10-08 13:31:12 -0400

    adjusted webpack-dev-server parameters in accordance with latest version

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,4 @@
 const { merge } = require('webpack-merge')
-const path = require('path')
 
 const common = require('./webpack.common')
 
@@ -7,9 +6,7 @@ module.exports = merge(common, {
 	mode: 'development',
 	devServer: {
 		port: 3000,
-		publicPath: '/',
-		contentBase: path.resolve('src'),
-		watchContentBase: true,
+		liveReload: true,
 		hot: true,
 		open: true
 	}
