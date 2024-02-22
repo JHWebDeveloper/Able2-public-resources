3ad6ac4e2139fb2979f2bd9830381377fa5974c4
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2024-02-21 20:29:28 -0500

    bumped Able2 version to 2.5.1 and SSG verion to 1.1.0

--- webpack.common.js
+++ webpack.common.js
@@ -4,10 +4,10 @@ const HTMLWebpackPlugin = require('html-webpack-plugin')
 const HTMLWebpackInlineSVGPlugin = require('html-webpack-inline-svg-plugin')
 const postcssPresetEnv = require('postcss-preset-env')
 
-const able2VersionWin = '2.5.0'
-const able2VersionMac = '2.5.0'
-const sSGVersionWin = '1.0.3'
-const sSGVersionMac = '1.0.3'
+const able2VersionWin = '2.5.1'
+const able2VersionMac = '2.5.1'
+const sSGVersionWin = '1.1.0'
+const sSGVersionMac = '1.1.0'
 
 module.exports = {
 	target: 'web',
